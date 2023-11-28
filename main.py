36d08272798436c0fb4416f1772ed19fced010fd
Tristan_Day <tmd526@gmail.com>
2023-11-28 01:12:35 -0600

    built send email code using environment variables and constructed message that will be sent to email.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "43f86ffc46984db794b257b903c78bf1"
 url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=43f86ffc46984db794b257b903c78bf1"
@@ -9,7 +10,15 @@ request = requests.get(url)
 # Get a dictionary in JSON format
 content = request.json()
 
-# Access the article titles and description
+# Send an email with news titles, descriptions and url
+raw_message = f"""\
+Subject: Recent techcrunch headlines
+
+"""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    raw_message += article['title'] + "\n"
+    if article['description'] is not None:
+        raw_message += article['description'] + "\n"
+    raw_message += article['url'] + "\n\n"
+
+send_email(raw_message.encode('utf-8'))
