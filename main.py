0538a7f94991dcfe747fb7044c0dfe06bc704e03
Tristan_Day <tmd526@gmail.com>
2023-11-29 01:31:41 -0600

    removed hard coded topic q value and fixed for loop to limit to first 10 items in content dict

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "43f86ffc46984db794b257b903c78bf1"
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=43f86ffc46984db794b257b903c78bf1"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "apiKey=43f86ffc46984db794b257b903c78bf1&"
+       "language=en")
 
 # Make a request with newsapi
 request = requests.get(url)
@@ -12,13 +17,14 @@ content = request.json()
 
 # Send an email with news titles, descriptions and url
 raw_message = f"""\
-Subject: Recent techcrunch headlines
+Subject: Today's news
 
 """
-for article in content['articles']:
-    raw_message += article['title'] + "\n"
-    if article['description'] is not None:
-        raw_message += article['description'] + "\n"
-    raw_message += article['url'] + "\n\n"
+for article in content['articles'][:10]:
+    if article['title'] is not None:
+        raw_message += article['title'] + "\n"
+        if article['description'] is not None:
+            raw_message += article['description'] + "\n"
+        raw_message += article['url'] + "\n\n"
 
 send_email(raw_message.encode('utf-8'))
